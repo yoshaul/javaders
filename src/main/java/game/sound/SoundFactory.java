3c371957baccf5d2524e96216a9043877b9c6579
yossis <yoshaul@gmail.com>
2015-03-14 22:56:48 +0200

    SCA: Using internal Logger

--- src/main/java/game/sound/SoundFactory.java
+++ src/main/java/game/sound/SoundFactory.java
@@ -19,35 +19,27 @@
 package game.sound;
 
 import game.GameConstants;
+import game.util.Logger;
 import game.util.ResourceManager;
 
 import javax.sound.sampled.*;
-import java.applet.Applet;
-import java.applet.AudioClip;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 
 /**
- * The <code>SoundFactory</code> class is used to play sounds
- * in the game.
+ * The <code>SoundFactory</code> class is used to play sounds in the game.
  */
 public class SoundFactory {
-
     private static LineListener lineListener = new SimpleLineListener();
 
     /**
      * Play the sound file. This method is not blocking.
      *
-     * @param fileName File name with the sound (we serach in the
-     *                 sounds folder).
+     * @param fileName File name with the sound (we search in the sounds folder).
      */
     public static void playSound(String fileName) {
-
         try {
-
-//            File soundFile = new File(GameConstants.SOUNDS_DIR + fileName);
             InputStream is = ResourceManager.getResourceAsStream(
                     GameConstants.SOUNDS_DIR + fileName);
             BufferedInputStream bis = new BufferedInputStream(is);
@@ -56,41 +48,17 @@ public class SoundFactory {
             DataLine.Info info = new DataLine.Info(Clip.class, audioFormat);
             // Obtain a line 
             Clip clip = (Clip) AudioSystem.getLine(info);
-            // Open the line (aquire resources)
+            // Open the line (acquire resources)
             clip.open(ais);
             // Set the line event's listener
             clip.addLineListener(lineListener);
             // Start playing the playback
             clip.start();
 
-        } catch (UnsupportedAudioFileException uafe) {
-            uafe.printStackTrace();
-        } catch (LineUnavailableException lue) {
-            lue.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-
-    }
-
-    /**
-     * Play an audio clip in the "Applet" way (i.e., using the
-     * <code>Applet</code> class). This method blocks so we don't
-     * use it in the game.
-     *
-     * @param fileName Name of the audio clip file.
-     */
-    public static void playAppletClip(String fileName) {
-        try {
-            URL soundURL = ResourceManager.getResource(
-                    GameConstants.SOUNDS_DIR + fileName);
-            AudioClip clip = Applet.newAudioClip(soundURL);
-            clip.play();
-        } catch (Exception e) {
+        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+            Logger.exception(e);
         }
     }
-
-
 }
 
 
